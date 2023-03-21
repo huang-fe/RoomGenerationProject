8327d2faa4e72c23df4f14b4026609af16d1013f
Felicity H <felicityh15@gmail.com>
2023-03-20 19:28:26 -0700

    Floor plan progress

--- Assets/Scripts/GenerateFloorPlan.cs
+++ Assets/Scripts/GenerateFloorPlan.cs
@@ -24,26 +24,124 @@ public class GenerateFloorPlan : MonoBehaviour
         //make floor
         GameObject floor = Instantiate(cube, new Vector3(w/2, 0, l/2), Quaternion.identity);
         floor.transform.localScale = new Vector3(w, thickness, l);
-        instantiated.Add(floor); 
+        //instantiated.Add(floor); 
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("space")) {
-            generateVerticalWall(0.0f);
+            if (instantiated.Count > 0) {
+                foreach (GameObject o in instantiated) {
+                    Destroy(o);
+                }
+                instantiated.Clear();
+            }
+            var start = Random.Range(seg, 2 * seg);
+            goDown(new Vector3(0, 0, start));
         }
     }
 
     // pick random l, go down random dist, go left or right random dist
-    void generateVerticalWall(float x) {
-        var start = Random.Range(seg, l-seg);
-        var d = Random.Range(seg, w);
-        if (d > w-seg){
-            d = w;
-        }
-        GameObject wall = Instantiate(cube, new Vector3(x, 0, start), Quaternion.identity);
+    void goDown(Vector3 offset) { // x offset
+        var d = Random.Range(seg, 2 * seg);
+
+        var p = offset + new Vector3(d/2, 0, 0);
+        GameObject wall = Instantiate(cube, p, Quaternion.identity);
         wall.transform.localScale = new Vector3(d, h, thickness);
-        instantiated.Add(wall);     
+        instantiated.Add(wall);
+        p.x = offset.x + d;     
+
+        if (hitBounds(p)) {
+            return;
+        } else {
+            // pick a direction
+            var dir = Random.Range(0, 3);
+            if (dir == 0) {
+                goRight(p);
+            } else if (dir == 2){
+                goLeft(p);
+            } else {
+                goRight(p);
+                goLeft(p);
+            }
+        }
+    }
+
+    void goUp(Vector3 offset) { // go in x dir
+        var d = Random.Range(seg, 2 * seg);
+
+        var p = offset - new Vector3(d/2, 0, 0);
+        GameObject wall = Instantiate(cube, p, Quaternion.identity);
+        wall.transform.localScale = new Vector3(d, h, thickness);
+        instantiated.Add(wall);
+        p.x = offset.x - d;     
+
+        if (hitBounds(p)) {
+            return;
+        } else {
+            // pick a direction
+            var dir = Random.Range(0, 3);
+            if (dir == 0) {
+                goRight(p);
+            } else if (dir == 2){
+                goLeft(p);
+            } else {
+                goRight(p);
+                goLeft(p);
+            }
+        }
+    }
+
+    void goRight(Vector3 offset) { // go in z dir
+        var d = Random.Range(seg, 2 * seg);
+
+        var p = offset + new Vector3(0, 0, d/2);
+        GameObject wall = Instantiate(cube, p, Quaternion.identity);
+        wall.transform.localScale = new Vector3(thickness, h, d);
+        instantiated.Add(wall);
+        p.z = offset.z + d;     
+
+        if (hitBounds(p)) {
+            return;
+        } else {
+            var dir = Random.Range(0, 3);
+            if (dir == 0) {
+                goUp(p);
+            } else if (dir == 2){
+                goDown(p);
+            } else {
+                goUp(p);
+                goDown(p);
+            }
+        }
+    }
+
+    void goLeft(Vector3 offset) { // go in z dir
+        var d = Random.Range(seg, 2 * seg);
+
+        var p = offset - new Vector3(0, 0, d/2);
+        GameObject wall = Instantiate(cube, p, Quaternion.identity);
+        wall.transform.localScale = new Vector3(thickness, h, d);
+        instantiated.Add(wall);
+        p.z = offset.z - d;     
+
+        if (hitBounds(p)) {
+            return;
+        } else {
+            var dir = Random.Range(0, 3);
+            if (dir == 0) {
+                goUp(p);
+            } else if (dir == 2){
+                goDown(p);
+            } else {
+                goUp(p);
+                goDown(p);
+            }
+        }
+    }
+
+    bool hitBounds(Vector3 p) {
+        return (p.x <= 0 || p.z <= 0 || p.x >= w || p.z >= l);
     }
 }
